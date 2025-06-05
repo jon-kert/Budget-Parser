32be4bf4e206572ec0e928a1ba4056d4a7d49c29
Jonathan_Kertawidjaja <jonkert@umich.edu>
2025-06-05 17:49:59 -0400

    Updated DB

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,56 @@
 import test
+import hashlib
+import os
+import psycopg2
+from dotenv import load_dotenv
 
 #test.check_psycopg()
-test.DB_Connect()
+test.DB_Connect()   #-- all tables have been created - use as reference for executing SQL Commands
+
+def main_DB_Connect():
+    return False
+
+def login():
+    username = input("Enter Username: ")
+    password = input("Enter Password: ")
+
+    #Check if @ Exists
+    USER = os.getenv("user")
+    PASSWORD = os.getenv("password")
+    HOST = os.getenv("host")
+    PORT = os.getenv("port")
+    DBNAME = os.getenv("dbname")
+    try:
+        connection = psycopg2.connect(
+            user=USER,
+            password=PASSWORD,
+            host=HOST,
+            port=PORT,
+            dbname=DBNAME
+        )
+        cursor = connection.cursor()
+        
+        cursor.execute("""IF EXISTS (SELECT 1 FROM Users WHERE ID = %s)
+                                    THEN 'TRUE'
+                                    ELSE 'FALSE' 
+                                    END
+                                """, username)
+        U_exists = cursor.fetchone()
+        
+        cursor.execute("") #fetch ID associated with Username
+        ID = cursor.fetchone()
+        
+        hashed = hashPass(password)
+        cursor.execute("", hashed) # check if hash associated with ID is correct
+        P_correct = cursor.fetchone()
+        
+        cursor.execute("") #Get the secure ID
+        Secure_ID = cursor.fetchone()
+        
+        
+        #Pass the Secure_ID Into the transaction/homepage function
+        # Potential security risks - ultimately aim to keep ID only accessible through the backend
+
+    except Exception as e:
+        print(f"Failed to connect: {e}")    
+    
--- Backend/test.py
+++ Backend/test.py
@@ -1,7 +1,11 @@
+# Sandbox programs
+# For learning do incorporate certain features into our program
+
 import psycopg2
 from dotenv import load_dotenv
 import os
-    
+#import hashlib
+
 def DB_Connect():
     print("Executing db access test...")
     load_dotenv()
@@ -23,26 +27,58 @@ def DB_Connect():
             dbname=DBNAME
         )
         print("Connection successful!")
-    
+
         # Create a cursor to execute SQL queries
         cursor = connection.cursor()
-    
+
         # Example query
         cursor.execute("SELECT NOW();")
         result = cursor.fetchone()
         print("Current Time:", result)
+        
+        #cursor.execute("DROP TABLE Transactions;")
+        #cursor.execute("DROP TABLE hashedpasses;")
+        
+        #cursor.execute("DROP TABLE users;")
+        
 
+        #CREATE TYPE IF NOT EXISTS user_type AS ENUM ('Client', 'Admin');
         # IF NOT EXISTS so that we dont make duplicate "TestPerson" tables
         cursor.execute("""
-                    CREATE TABLE IF NOT EXISTS TestPerson (
-                    PersonID SERIAL PRIMARY KEY,
-                    LastName VARCHAR(255),
-                    FirstName VARCHAR(255),
-                    Address VARCHAR(255),
-                    City VARCHAR(255)
+                    CREATE TABLE IF NOT EXISTS Users (
+                    ID SERIAL PRIMARY KEY,
+                    username VARCHAR(255),
+                    account_created DATE,
+                    role user_type NOT NULL
                     );""")
         connection.commit()
-        print("Table Created! Check Supabase for Table")
+        print("Users table created...")
+        
+        # Planning on using SHA-1 hashing, which requires 40 characters of space
+        #Recheck if the ID is even necessary??
+        cursor.execute("""
+                       CREATE TABLE IF NOT EXISTS HashedPasses (
+                       ID INT PRIMARY KEY,
+                       hash CHAR(40),
+                       FOREIGN KEY (ID) REFERENCES Users(ID),
+                       secure_id INT UNIQUE
+                       );""")
+        connection.commit()
+        print("HashedPasses table created...")
+
+        #CREATE TYPE transaction_type AS ENUM ('out', 'in');
+        cursor.execute("""
+                       CREATE TABLE IF NOT EXISTS Transactions (
+                       TransactionID SERIAL PRIMARY KEY,
+                       user_id INT,
+                       transaction_date DATE,
+                       FOREIGN KEY (user_id) REFERENCES HashedPasses(secure_id),
+                       transaction_amount money,
+                       transaction_type transaction_type NOT NULL
+                       );""")
+        connection.commit()
+        print("Transactions table created...")
+        print("All tables created successfully! Check Supabase for Tables")
 
         # Close the cursor and connection
         cursor.close()
@@ -56,3 +92,16 @@ def check_psycopg():
     print("Module path:", psycopg2.__file__)
     print("Type:", type(psycopg2))
     print("Has attribute cursor?", hasattr(psycopg2, "cursor"))
+    
+def create_account():
+    username = input("Enter a Username: ")
+    name = input("Enter your full name: ")
+    password = input("Enter a password: ")
+    #Access the db, and check if the username exists. If so, do not make the account
+    
+    #if does not yet exist, 
+    
+    
+    print("account created!")
+
+#def access_account():
